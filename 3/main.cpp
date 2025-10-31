8249f44bb3a7288734bd6ac6e56da2e606800067
yoru <tfchanal@gmail.com>
2025-10-31 11:33:15 +0300

    #3

--- 3/main.cpp
+++ 3/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv)
   std::string s;
   std::cin >> s;
   std::stack<std::pair<u_int16_t, char>> stack;
+
   for(u_int16_t i = 0; i < s.size(); i++) {
       if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
         stack.push(std::make_pair(i+1, s[i]));
@@ -18,7 +19,7 @@ int main(int argc, char** argv)
         if(stack.top().second == '[') stack.pop();
         else
         {
-          std::cout << stack.top().second << "\n";
+          std::cout << i+1 << "\n";
           return 0;
         }
       }
@@ -27,7 +28,7 @@ int main(int argc, char** argv)
         if(stack.top().second == '{') stack.pop();
         else
         {
-          std::cout << stack.top().second << "\n";
+          std::cout << i+1 << "\n";
           return 0;
         }
       }
@@ -36,12 +37,24 @@ int main(int argc, char** argv)
         if(stack.top().second == '(') stack.pop();
         else
         {
-          std::cout << stack.top().second << "\n";
+          std::cout << i+1 << "\n";
           return 0;
         }
       }
 
       
   }
+  if(stack.empty()) {
+    std::cout << "SUCCESS" << std::endl;
+    return 0;
+  }
+    auto it = stack.top();
+  
+  while(!stack.empty()) {
+      it = stack.top();
+      stack.pop();
+  }
+  std::cout << it.first << std::endl;
   return 0;
+
 }
